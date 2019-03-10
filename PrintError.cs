6a8341d5de937632df5c0a3daef765850a3ef28c
waraporn maensiri <wara.phon@odds.team>
2019-03-10 13:44:30 +0700

    Complete Kata: PrintError

--- PrintError.cs
+++ PrintError.cs
@@ -13,16 +13,17 @@ namespace Count_characters_in_your_string
             string color = "abcdefghijklm";
             int countColor = 0;
             var output = cha.GroupBy(word => word).Select(group => group.Key).ToList();
-
+           
             foreach (char ch in output)
             {
                 if (!color.Contains(ch))
                 {
-                    Console.WriteLine("---" + ch);
-                    countColor++;
+                    //  Console.WriteLine("ch = "+ ch);
+                    int count = s.Where(w => w.Equals(ch)).Count();
+                    // Console.WriteLine("count = "+ count);
+                    countColor += count;
                 }
             }
-
             return countColor + "/" + s.Length;
         }
     }
